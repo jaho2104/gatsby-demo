c0656f74f86d4878faa1a99e694b5f43c45326d7
jaho <jaho2104@gmail.com>
2019-04-22 12:56:20 -0500

    Add paragraph in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,11 @@ import Layout from "../components/layout"
 const IndexPage = () => (
     <Layout>
         <Head title="Home" />
-        <h1>Hello Gatsby!</h1>
+        <h2>Hello Gatsby!</h2>
+        <p>
+            I'm Antonio, this is a demo while playground learning the great
+            Gatsby!
+        </p>
     </Layout>
 )
 
